a0f8093929ccba1879b570d2de7cfc39dc5b7b0b
May Kong <1285235080@qq.com>
2018-12-16 12:06:07 +1300

    add the other class to main app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {createStore} from 'redux';
+import React, {Component} from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+class App extends React.Component{
+  render(){
+    const boss = '李云龙'
+    //instead of the html tag, <h2> is a chunk of jsx syntax
+    return <h2>独立团， 团长{boss}</h2>
+  }
+}
+class FirstArmy extends React.Component{
+  render(){
+    const boss = '张大飚'
+    return <h2>一营营长， 营长{boss}</h2>
   }
 }
 
-export default App;
+export default App
