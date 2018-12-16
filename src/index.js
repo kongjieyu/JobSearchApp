e470cdef01293410997bdbaa04e398302a105745
May Kong <1285235080@qq.com>
2018-12-16 20:29:02 +1300

    init Redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//新建store
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//新建reducer
+//根据老的状态生成新的状态
+//每个aciton时有type的
+function counter(state = 0, action) {
+    switch(action.type){
+        case '加机关枪':
+            return state + 1
+        case '减机关枪':
+            return state - 1
+        default: 
+            return 10
+    }
+}
+//括号里面要放reducer
+const store = createStore(counter)
+//初始状态
+const init = store.getState()
+console.log(init)
+store.dispatch({type: '加机关枪'})
+const secondState = store.getState()
+console.log(secondState)
+store.dispatch({type: '加机关枪'})
+const thirdState = store.getState()
+console.log(thirdState)
