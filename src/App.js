0b4851b82be4692ea5a25d316f17f7c66ff5889e
May Kong <1285235080@qq.com>
2018-12-16 19:26:13 +1300

    add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import { Button, List } from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.css';
 
 class App extends React.Component{
   render(){
@@ -35,12 +37,31 @@ class FirstArmy extends React.Component{
     this.state = {
       solders: ['虎子','二子','陈达子']
     }
+    //bind（）;因为下方button中this.addSolders触发后，其实它是找不addSholder中的this的，这个时候需要我们绑定一下
+    //this.addSolders = this.addSolders.bind(this)
   }
+  componentWillMount() {
+    console.log('组件马上就要加载')
+  }
+  componentDidMount() {
+    console.log('组件加载完毕')
+  }
+  addSolders(){
+    console.log('组件在add里加载')
+    console.log('Add new Solders')
+    this.setState({
+      // 拼接数组用逗号
+      solders: [...this.state.solders, '新兵蛋子' + Math.random()]
+    })
+  }
+
   render(){
+    console.log('组件正在加载')
     const boss = '张大飚'
     return (  
       <div>
         <h2>一营营长， 营长{boss}，副营长{this.props.boss2}</h2>
+        <Button type="primary" onClick={()=>this.addSolders()}>新兵入伍</Button>
         <ul>
           {this.state.solders.map(v=>{
             return <li key={v}>{v}</li>
@@ -48,8 +69,6 @@ class FirstArmy extends React.Component{
         </ul>
       </div>
     )
-
-
   }
 }
 
