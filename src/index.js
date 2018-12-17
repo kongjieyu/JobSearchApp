f61faa3ad1dd2f3fc08c1de921ad542da2225db6
May Kong <1285235080@qq.com>
2018-12-17 13:35:21 +1300

    index.js File add ReduxDevTools

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import App from './App'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 //import reducer
 import { counter, addGun, removeGun, addGunAsync } from './index.redux'
 
+const reduxDevtools = window.devToolsExtension?window.devToolsExtension():f=>f
+
 //这一步就将reduce放进了store里面了
 //applyMiddleware加在reducer后面,把thunk作为参数传人
-const store = createStore(counter, applyMiddleware(thunk))
+const store = createStore(counter, compose(
+    applyMiddleware(thunk),
+    reduxDevtools
+))
+
+
 
 function render() {
     //App既react文件
