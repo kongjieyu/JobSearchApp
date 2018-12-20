7b2efb0c7a40c8cd4e9c9dc1c56710b1eb68c3ec
May Kong <1285235080@qq.com>
2018-12-21 11:13:23 +1300

    modify the url

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -22,6 +22,8 @@ class Dashboard extends React.Component{
     }
     render(){
         console.log(this.props)
+        const match = this.props.match
+        console.log(match)
         const redirectToLogin = <Redirect to='/login'></Redirect>
         const app = (
         <div>
@@ -29,18 +31,19 @@ class Dashboard extends React.Component{
             {this.props.isAuth? <button onClick={this.props.logout}>注销</button>: null}
             <ul>
                 <li>
-                    <Link to='/dashboard/'>一营</Link>
+                    {/* <Link to='/dashboard/'>一营</Link> :this can change into */}
+                    <Link to={`${match.url}/`}>一营</Link>
                 </li>
                 <li>
-                    <Link to='/dashboard/erying'>二营</Link>
+                    <Link to={`${match.url}/erying`}>二营</Link>
                 </li>
                 <li>
-                    <Link to='/dashboard/qibinglian'>骑兵连</Link>
+                    <Link to={`${match.url}/qibinglian`}>骑兵连</Link>
                 </li>
             </ul>
-            <Route path='/dashboard/' exact component={App}></Route>
-            <Route path='/dashboard/erying' component={Erying}></Route>
-            <Route path='/dashboard/qibinglian' component={Qibinglian}></Route>
+            <Route path={`${match.url}/`} exact component={App}></Route>
+            <Route path={`${match.url}/erying`} component={Erying}></Route>
+            <Route path={`${match.url}/qibinglian`} component={Qibinglian}></Route>
         </div>
         )
         return this.props.isAuth? app: redirectToLogin
